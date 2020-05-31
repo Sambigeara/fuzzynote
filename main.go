bbf09b24eb407104fffed64a5f673a3f6db2fc94
Sam Lock <sam@swlock.co.uk>
2020-05-31 18:18:21 +0100

    Add initial stdscr struct for stdout terminal output

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bufio"
-    "errors"
+	"errors"
 	"fmt"
 	"log"
 	"os"
-    "os/exec"
+	"os/exec"
 	"time"
-    "unicode"
+	"unicode"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -17,7 +17,7 @@ import (
 const Root = "rootPage"
 
 type PageItem struct {
-	Line       string
+	Line      string
 	DtCreated time.Time
 }
 
@@ -43,83 +43,101 @@ func loadRootPage() []PageItem {
 	return page
 }
 
-type searchString struct {
-    Key []rune
+type SearchString struct {
+	Key []rune
 }
 
 func clearScreen() {
-    // TODO cross platform screen-cleaning, or figure out how to clear stdout in place
-    cmd := exec.Command("clear")
-    cmd.Stdout = os.Stdout
-    cmd.Run()
+	// TODO cross platform screen-cleaning, or figure out how to clear stdout in place
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
 
-func (s *searchString) ProcessKeyPress(r rune) error {
-    if unicode.IsLetter(r) || unicode.IsNumber(r) {
-        s.Key = append(s.Key, r)
-    } else if r == '\u007f' {
-        // Delete removes last item from slice
-        if len(s.Key) > 0 {
-            s.Key = s.Key[:len(s.Key)-1]
-        }
-    } else if r == '\x1b' {
-        return errors.New("Esc: exiting...")
-    }
-    clearScreen()
-    return nil
+func (s *SearchString) ProcessKeyPress(r rune) error {
+	if unicode.IsLetter(r) || unicode.IsNumber(r) {
+		s.Key = append(s.Key, r)
+	} else if r == '\u007f' {
+		// Delete removes last item from slice
+		if len(s.Key) > 0 {
+			s.Key = s.Key[:len(s.Key)-1]
+		}
+	} else if r == '\x1b' {
+		return errors.New("Esc: exiting...")
+	}
+	clearScreen()
+	return nil
 }
 
-func (s *searchString) FetchMatches(page []PageItem) ([]string, error) {
-    res := []string{}
-    for _, p := range page {
-        if IsFuzzyMatch(string(s.Key), p.Line) {
-            res = append(res, p.Line)
-        }
-    }
-    return res, nil // TODO
+// TODO move method from SearchString
+func (s *SearchString) FetchMatches(page []PageItem) ([]PageItem, error) {
+	res := []PageItem{}
+	for _, p := range page {
+		if IsFuzzyMatch(string(s.Key), p.Line) {
+			res = append(res, p)
+		}
+	}
+	return res, nil // TODO
+}
+
+// TODO this will represent the painted output with data and cursor position
+type StdScr struct {
+	SearchString []SearchString
+	Lines        []PageItem
+	CursX        int
+	CursY        int
+	LenX         int
+	LenY         int
+}
+
+func (scr *StdScr) Paint() {
 }
 
 func main() {
-    page := loadRootPage()
+	page := loadRootPage()
 
-    //https://godoc.org/golang.org/x/crypto/ssh/terminal
-    oldState, err := terminal.MakeRaw(0)
-    if err != nil {
-            panic(err)
-    }
-    defer terminal.Restore(0, oldState)
+	//https://godoc.org/golang.org/x/crypto/ssh/terminal
+	oldState, err := terminal.MakeRaw(0)
+	if err != nil {
+		panic(err)
+	}
+	defer clearScreen()
+	defer terminal.Restore(0, oldState)
 
-    clearScreen()
+	clearScreen()
 
 	reader := bufio.NewReader(os.Stdin)
-    s := searchString{}
+	s := SearchString{}
 	for {
-        // Retrieve single byte
-        //ascii, _ := reader.ReadByte()
-        //fmt.Print(ascii)
+		// Retrieve single byte
+		//ascii, _ := reader.ReadByte()
+		//fmt.Print(ascii)
 
-        // Retrieve single UTF-8 encoded Unicode char
-        // TODO at the mo many buttons start with escape sequence \x1b, so many chars trigger exit
+		// Retrieve single UTF-8 encoded Unicode char
+		// TODO at the mo many buttons start with escape sequence \x1b, so many chars trigger exit
 		r, _, err := reader.ReadRune()
 		if err != nil {
 			log.Println("stdin:", err)
 			break
 		}
-        err = s.ProcessKeyPress(r)
-        if err != nil {
+		err = s.ProcessKeyPress(r)
+		if err != nil {
 			log.Println("stdin:", err)
 			break
-        }
+		}
 
-        matches, err := s.FetchMatches(page)
-        if err != nil {
+		matches, err := s.FetchMatches(page)
+		if err != nil {
 			log.Println("stdin:", err)
 			break
-        }
+		}
 
-        fmt.Printf("%s\r", string(s.Key))
-        for _, r := range matches {
-            fmt.Printf("%s\n", r)
-        }
+		fmt.Printf("%s\n\r", string(s.Key))
+		for _, r := range matches {
+			fmt.Printf("%s\n\r", r.Line)
+		}
+		// Set cursor position
+		line, col := 1, 1
+		fmt.Printf("\033[%d;%dH", line, col)
 	}
 }
