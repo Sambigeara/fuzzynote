2f6380e168688fe1bd18cee43f683b763c6b3af1
Sambigeara <sam@swlock.co.uk>
2020-06-03 09:46:08 +0100

    Refactored to use tcell lib

--- term.go
+++ term.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"golang.org/x/crypto/ssh/terminal"
 	"log"
 	"os"
-	"os/exec"
-	"unicode"
+
+	"github.com/gdamore/tcell"
+	"github.com/gdamore/tcell/encoding"
+	"github.com/mattn/go-runewidth"
 )
+
+var defStyle tcell.Style
 
 type SearchString struct {
 	Key []rune
@@ -21,74 +22,83 @@ type StdScr struct {
 	CurPos int
 }
 
-func (scr *StdScr) clearScreen() {
-	// TODO cross platform screen-cleaning, or figure out how to clear stdout in place
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-}
-
-func (scr *StdScr) ProcessKeyPress(r rune) error {
-	s := &scr.Search
-	if unicode.IsLetter(r) || unicode.IsNumber(r) {
-		s.Key = append(s.Key, r)
-	} else if r == '\u007f' {
-		// Delete removes last item from slice
-		if len(s.Key) > 0 {
-			s.Key = s.Key[:len(s.Key)-1]
+func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
+	for _, c := range str {
+		var comb []rune
+		w := runewidth.RuneWidth(c)
+		if w == 0 {
+			comb = []rune{c}
+			c = ' '
+			w = 1
 		}
-	} else if r == '\x1b' {
-		return errors.New("Esc: exiting...")
+		s.SetContent(x, y, c, comb, style)
+		x += w
 	}
-	scr.clearScreen()
-
-	// Set cursor highlight position
-    //col := len(s.Key)
-    //fmt.Printf("\033[%d;%dH", 1, col)
-    fmt.Printf("\033[%d;%dH", 1, 1)
-
-	return nil
-}
-
-func (scr *StdScr) Paint(matches []PageItem) error {
-	fmt.Printf("%s\n\r", string(scr.Search.Key))
-	for _, r := range matches {
-		fmt.Printf("%s\n\r", r.Line)
-	}
-	return nil
 }
 
 func (scr *StdScr) HandleKeyPresses(db DbRepo) {
-    oldState, err := terminal.MakeRaw(0)
-    if err != nil {
-            panic(err)
-    }
-    defer terminal.Restore(0, oldState)
-	scr.clearScreen()
 
-	reader := bufio.NewReader(os.Stdin)
+	encoding.Register()
+
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e := s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	defStyle = tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	s.SetStyle(defStyle)
+	s.EnableMouse()
+	s.Clear()
+
+	//white := tcell.StyleDefault.
+	//    Foreground(tcell.ColorWhite).Background(tcell.ColorRed)
+
+	w, h := s.Size()
+
+	//emitStr(s, 1, 1, white, "Welcome to fuzzy-note. Press Ctl-C to exit.")
+
 	for {
-		// Retrieve single byte
-		//ascii, _ := reader.ReadByte()
-		//fmt.Print(ascii)
+		s.Show()
+		ev := s.PollEvent()
+		w, h = s.Size()
+		search := &scr.Search
 
-		// Retrieve single UTF-8 encoded Unicode char
-		// TODO at the mo many buttons start with escape sequence \x1b, so many chars trigger exit
-		r, _, err := reader.ReadRune()
+		// https://github.com/gdamore/tcell/blob/master/_demos/mouse.go
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			s.SetContent(w-1, h-1, ev.Rune(), nil, defStyle)
+			switch ev.Key() {
+			case tcell.KeyCtrlC:
+				s.Fini()
+				os.Exit(0)
+			case tcell.KeyEscape:
+				search.Key = []rune{}
+			case tcell.KeyBackspace:
+			case tcell.KeyBackspace2:
+				// Delete removes last item from slice
+				if len(search.Key) > 0 {
+					search.Key = search.Key[:len(search.Key)-1]
+				}
+			default:
+				search.Key = append(search.Key, ev.Rune())
+			}
+		}
+		s.Clear()
+
+		matches, err := db.FetchMatches(search.Key)
 		if err != nil {
 			log.Println("stdin:", err)
 			break
 		}
-		err = scr.ProcessKeyPress(r)
-		if err != nil {
-			log.Println("stdin:", err)
-			break
+		emitStr(s, 0, 0, defStyle, fmt.Sprint(string(search.Key)))
+		for i, r := range matches {
+			emitStr(s, 0, i+1, defStyle, r.Line)
 		}
-		matches, err := db.FetchMatches(scr.Search.Key)
-		if err != nil {
-			log.Println("stdin:", err)
-			break
-		}
-		scr.Paint(matches)
 	}
 }
