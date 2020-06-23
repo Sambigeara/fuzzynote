bb0482532bff89e2d62fd7ec5ed3cfa6b059f7cb
Sam Lock <sam@swlock.co.uk>
2020-06-23 07:57:38 +0100

    Refactor into separate component packages (#2)
    There are now distinct "service" and "client" packages. This was done to
    clean up the boundaries between the components, and to offer clean
    interfaces from one to the other.

---
+++ cmd/fuzzy-note/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"fuzzy-note/pkg/client"
+	"fuzzy-note/pkg/service"
+	//"github.com/Sambigeara/fuzzy-note/pkg/client"
+	//"github.com/Sambigeara/fuzzy-note/pkg/service"
+)
+
+func main() {
+	//ImportLines()
+
+	var rootPath string
+	if rootPath = os.Getenv("ROOT_PAGE"); rootPath == "" {
+		rootPath = "pages/root"
+	}
+
+	listRepo := service.NewDBListRepo(rootPath)
+
+	//list, err := db.Get()
+	//if err != nil {
+	//    log.Error(err)
+	//}
+
+	term := client.NewTerm(listRepo)
+
+	err := term.RunClient()
+	// TODO
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//list, err = p.HandleKeyPresses()
+	//if err != nil {
+	//    log.Error(err)
+	//} else {
+	//    db.Store()
+	//}
+
+	// TODO this should be handled here gracefully (rather than in loop as current)
+	//p.StoreList(RootPath)
+}
