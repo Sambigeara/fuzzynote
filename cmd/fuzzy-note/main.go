16ba201957c9fd5133a609d22e5e1c4ee869a0d0
Sam Lock <sam@swlock.co.uk>
2020-07-01 21:19:52 +0100

    List item notes support. Various fixes. (#5)
    Add support to create and maintain notes for list items.
    Various other client fixes and improvements

--- cmd/fuzzy-note/main.go
+++ cmd/fuzzy-note/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path"
 
 	"fuzzy-note/pkg/client"
 	"fuzzy-note/pkg/service"
@@ -10,13 +11,25 @@ import (
 	//"github.com/Sambigeara/fuzzy-note/pkg/service"
 )
 
+const rootFileName = "primary.db"
+
 func main() {
-	var rootPath string
-	if rootPath = os.Getenv("FZN_ROOT_PAGE"); rootPath == "" {
-		rootPath = "pages/root"
+	var rootDir, notesSubDir string
+	if rootDir = os.Getenv("FZN_ROOT_DIR"); rootDir == "" {
+		// TODO currently only works on OSs with HOME
+		rootDir = path.Join(os.Getenv("HOME"), ".fzn/")
+	}
+	if notesSubDir = os.Getenv("FZN_NOTES_SUBDIR"); notesSubDir == "" {
+		notesSubDir = "notes"
 	}
 
-	listRepo := service.NewDBListRepo(rootPath)
+	rootPath := path.Join(rootDir, rootFileName)
+	notesDir := path.Join(rootDir, notesSubDir)
+
+	// Create (if not exists) the notes subdirectory
+	os.MkdirAll(notesDir, os.ModePerm)
+
+	listRepo := service.NewDBListRepo(rootPath, notesDir)
 
 	term := client.NewTerm(listRepo)
 
