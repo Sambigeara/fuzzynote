65598983e94ed1af1d66ce42d94fd536e48e1ec7
Sam Lock <sam@swlock.co.uk>
2021-03-18 21:47:53 +0000

    Remove list item ID (#35)
    * Refactored out list item IDs in place of Unix time nano timestamps for better uniqueness guarantees

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -51,15 +51,15 @@ type DBListRepo struct {
 // ListItem represents a single item in the returned list, based on the Match() input
 type ListItem struct {
 	// TODO these can all be private now
-	Line        string
-	Note        *[]byte
-	IsHidden    bool
-	originUUID  uuid
-	child       *ListItem
-	parent      *ListItem
-	id          uint64
-	matchChild  *ListItem
-	matchParent *ListItem
+	Line         string
+	Note         *[]byte
+	IsHidden     bool
+	originUUID   uuid
+	creationTime int64
+	child        *ListItem
+	parent       *ListItem
+	matchChild   *ListItem
+	matchParent  *ListItem
 }
 
 type bits uint32
@@ -85,16 +85,16 @@ func NewDBListRepo(rootDir string, walFiles []WalFile) *DBListRepo {
 	}
 }
 
-func (r *DBListRepo) processEventLog(e eventType, id uint64, targetID uint64, newLine string, newNote *[]byte, originUUID uuid, targetUUID uuid) error {
+func (r *DBListRepo) processEventLog(e eventType, creationTime int64, targetCreationTime int64, newLine string, newNote *[]byte, originUUID uuid, targetUUID uuid) error {
 	el := EventLog{
-		uuid:             originUUID,
-		targetUUID:       targetUUID,
-		unixNanoTime:     time.Now().UnixNano(),
-		eventType:        e,
-		listItemID:       id,
-		targetListItemID: targetID,
-		line:             newLine,
-		note:             newNote,
+		eventType:                  e,
+		uuid:                       originUUID,
+		targetUUID:                 targetUUID,
+		unixNanoTime:               time.Now().UnixNano(),
+		listItemCreationTime:       creationTime,
+		targetListItemCreationTime: targetCreationTime,
+		line:                       newLine,
+		note:                       newNote,
 	}
 	*r.wal.log = append(*r.wal.log, el)
 	var err error
@@ -109,7 +109,7 @@ func (r *DBListRepo) Add(line string, note *[]byte, idx int) error {
 		return fmt.Errorf("ListItem idx out of bounds: %v", idx)
 	}
 
-	var childID uint64
+	var childCreationTime int64
 	// In order to be able to resolve child node from the tracker mapping, we need UUIDs to be consistent
 	// Therefore, whenever we reference a child, we need to set the originUUID to be consistent
 	// This is (conveniently) in line with intended behaviour for now. If we merge a remote, it makes sense
@@ -118,12 +118,13 @@ func (r *DBListRepo) Add(line string, note *[]byte, idx int) error {
 	newUUID := r.wal.uuid
 	if idx > 0 {
 		childItem := r.matchListItems[idx-1]
-		childID = childItem.id
+		childCreationTime = childItem.creationTime
 		newUUID = childItem.originUUID
 	}
-	newID := r.NextID
-	r.processEventLog(addEvent, newID, childID, line, note, newUUID, newUUID)
-	r.addUndoLog(addEvent, newID, childID, newUUID, newUUID, line, note, line, note)
+	// TODO ideally we'd use the same unixtime for log creation and the listItem creation time for Add()
+	now := time.Now().UnixNano()
+	r.processEventLog(addEvent, now, childCreationTime, line, note, newUUID, newUUID)
+	r.addUndoLog(addEvent, now, childCreationTime, newUUID, newUUID, line, note, line, note)
 	return nil
 }
 
@@ -136,8 +137,8 @@ func (r *DBListRepo) Update(line string, note *[]byte, idx int) error {
 	listItem := r.matchListItems[idx]
 
 	// Add the UndoLog here to allow us to access existing Line/Note state
-	r.addUndoLog(updateEvent, listItem.id, 0, listItem.originUUID, listItem.originUUID, listItem.Line, listItem.Note, line, note)
-	r.processEventLog(updateEvent, listItem.id, 0, line, note, listItem.originUUID, uuid(0))
+	r.addUndoLog(updateEvent, listItem.creationTime, 0, listItem.originUUID, listItem.originUUID, listItem.Line, listItem.Note, line, note)
+	r.processEventLog(updateEvent, listItem.creationTime, 0, line, note, listItem.originUUID, uuid(0))
 	return nil
 }
 
@@ -149,14 +150,14 @@ func (r *DBListRepo) Delete(idx int) error {
 
 	listItem := r.matchListItems[idx]
 
-	var targetListItemID uint64
+	var targetCreationTime int64
 	var targetUUID uuid
 	if listItem.child != nil {
-		targetListItemID = listItem.child.id
+		targetCreationTime = listItem.child.creationTime
 		targetUUID = listItem.child.originUUID
 	}
-	r.processEventLog(deleteEvent, listItem.id, 0, "", nil, listItem.originUUID, uuid(0))
-	r.addUndoLog(deleteEvent, listItem.id, targetListItemID, listItem.originUUID, targetUUID, listItem.Line, listItem.Note, listItem.Line, listItem.Note)
+	r.processEventLog(deleteEvent, listItem.creationTime, 0, "", nil, listItem.originUUID, uuid(0))
+	r.addUndoLog(deleteEvent, listItem.creationTime, targetCreationTime, listItem.originUUID, targetUUID, listItem.Line, listItem.Note, listItem.Line, listItem.Note)
 	return nil
 }
 
@@ -169,24 +170,24 @@ func (r *DBListRepo) MoveUp(idx int) (bool, error) {
 
 	listItem := r.matchListItems[idx]
 
-	var targetItemID uint64
+	var targetCreationTime int64
 	var targetUUID uuid
 	if listItem.matchChild != nil {
-		targetItemID = listItem.matchChild.id
+		targetCreationTime = listItem.matchChild.creationTime
 		targetUUID = listItem.matchChild.originUUID
 	} else if listItem.child != nil {
 		// Cover nil child case (e.g. attempting to move top of list up)
 
 		// matchChild will only be null in this context on initial startup with loading
 		// from the WAL
-		targetItemID = listItem.child.id
+		targetCreationTime = listItem.child.creationTime
 		targetUUID = listItem.child.originUUID
 	}
 
-	r.processEventLog(moveUpEvent, listItem.id, targetItemID, "", nil, listItem.originUUID, targetUUID)
+	r.processEventLog(moveUpEvent, listItem.creationTime, targetCreationTime, "", nil, listItem.originUUID, targetUUID)
 	// There's no point in moving if there's nothing to move to
-	if targetItemID != 0 {
-		r.addUndoLog(moveUpEvent, listItem.id, targetItemID, listItem.originUUID, targetUUID, "", nil, "", nil)
+	if targetCreationTime != 0 {
+		r.addUndoLog(moveUpEvent, listItem.creationTime, targetCreationTime, listItem.originUUID, targetUUID, "", nil, "", nil)
 		return true, nil
 	}
 	return false, nil
@@ -201,20 +202,20 @@ func (r *DBListRepo) MoveDown(idx int) (bool, error) {
 
 	listItem := r.matchListItems[idx]
 
-	var targetItemID uint64
+	var targetCreationTime int64
 	var targetUUID uuid
 	if listItem.matchParent != nil {
-		targetItemID = listItem.matchParent.id
+		targetCreationTime = listItem.matchParent.creationTime
 		targetUUID = listItem.matchParent.originUUID
 	} else if listItem.parent != nil {
-		targetItemID = listItem.parent.id
+		targetCreationTime = listItem.parent.creationTime
 		targetUUID = listItem.parent.originUUID
 	}
 
-	r.processEventLog(moveDownEvent, listItem.id, targetItemID, "", nil, listItem.originUUID, targetUUID)
+	r.processEventLog(moveDownEvent, listItem.creationTime, targetCreationTime, "", nil, listItem.originUUID, targetUUID)
 	// There's no point in moving if there's nothing to move to
-	if targetItemID != 0 {
-		r.addUndoLog(moveDownEvent, listItem.id, targetItemID, listItem.originUUID, targetUUID, "", nil, "", nil)
+	if targetCreationTime != 0 {
+		r.addUndoLog(moveDownEvent, listItem.creationTime, targetCreationTime, listItem.originUUID, targetUUID, "", nil, "", nil)
 		return true, nil
 	}
 	return false, nil
@@ -231,12 +232,12 @@ func (r *DBListRepo) ToggleVisibility(idx int) error {
 	var evType eventType
 	if listItem.IsHidden {
 		evType = showEvent
-		r.addUndoLog(showEvent, listItem.id, 0, listItem.originUUID, listItem.originUUID, "", nil, "", nil)
+		r.addUndoLog(showEvent, listItem.creationTime, 0, listItem.originUUID, listItem.originUUID, "", nil, "", nil)
 	} else {
 		evType = hideEvent
-		r.addUndoLog(hideEvent, listItem.id, 0, listItem.originUUID, listItem.originUUID, "", nil, "", nil)
+		r.addUndoLog(hideEvent, listItem.creationTime, 0, listItem.originUUID, listItem.originUUID, "", nil, "", nil)
 	}
-	r.processEventLog(evType, listItem.id, 0, "", nil, listItem.originUUID, uuid(0))
+	r.processEventLog(evType, listItem.creationTime, 0, "", nil, listItem.originUUID, uuid(0))
 	return nil
 }
 
@@ -245,7 +246,7 @@ func (r *DBListRepo) Undo() error {
 		// undo event log
 		uel := r.eventLogger.log[r.eventLogger.curIdx]
 
-		r.processEventLog(oppositeEvent[uel.eventType], uel.listItemID, uel.targetListItemID, uel.undoLine, uel.undoNote, uel.uuid, uel.targetUUID)
+		r.processEventLog(oppositeEvent[uel.eventType], uel.listItemCreationTime, uel.targetListItemCreationTime, uel.undoLine, uel.undoNote, uel.uuid, uel.targetUUID)
 		var err error
 		r.eventLogger.curIdx--
 		return err
@@ -258,7 +259,7 @@ func (r *DBListRepo) Redo() error {
 	if r.eventLogger.curIdx < len(r.eventLogger.log)-1 {
 		uel := r.eventLogger.log[r.eventLogger.curIdx+1]
 
-		r.processEventLog(uel.eventType, uel.listItemID, uel.targetListItemID, uel.redoLine, uel.redoNote, uel.uuid, uel.targetUUID)
+		r.processEventLog(uel.eventType, uel.listItemCreationTime, uel.targetListItemCreationTime, uel.redoLine, uel.redoNote, uel.uuid, uel.targetUUID)
 		var err error
 		r.eventLogger.curIdx++
 		return err
--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -44,13 +44,13 @@ func TestServiceStoreLoad(t *testing.T) {
 		defer clearUp(repo)
 
 		oldItem := ListItem{
-			Line: "Old newly created line",
-			id:   uint64(1),
+			Line:         "Old newly created line",
+			creationTime: int64(1),
 		}
 		newItem := ListItem{
-			Line:   "New newly created line",
-			parent: &oldItem,
-			id:     uint64(2),
+			Line:         "New newly created line",
+			parent:       &oldItem,
+			creationTime: int64(2),
 		}
 		oldItem.child = &newItem
 
@@ -82,18 +82,18 @@ func TestServiceStoreLoad(t *testing.T) {
 			t.Errorf("File schema %d: Expected %s but got %s", repo.latestFileSchemaID, newItem.Line, root.Line)
 		}
 
-		expectedID := uint64(2)
-		if root.id != expectedID {
-			t.Errorf("File schema %d: Expected %d but got %d", repo.latestFileSchemaID, expectedID, root.id)
+		expectedID := int64(2)
+		if root.creationTime != expectedID {
+			t.Errorf("File schema %d: Expected %d but got %d", repo.latestFileSchemaID, expectedID, root.creationTime)
 		}
 
 		if root.parent.Line != oldItem.Line {
 			t.Errorf("File schema %d: Expected %s but got %s", repo.latestFileSchemaID, root.parent.Line, oldItem.Line)
 		}
 
-		expectedID = uint64(1)
-		if root.parent.id != expectedID {
-			t.Errorf("File schema %d: Expected %d but got %d", repo.latestFileSchemaID, expectedID, root.parent.id)
+		expectedID = int64(1)
+		if root.parent.creationTime != expectedID {
+			t.Errorf("File schema %d: Expected %d but got %d", repo.latestFileSchemaID, expectedID, root.parent.creationTime)
 		}
 	})
 }
@@ -127,11 +127,6 @@ func TestServiceAdd(t *testing.T) {
 
 		if newItem != item1.child {
 			t.Errorf("New item should be original root's child")
-		}
-
-		expectedID := uint64(3)
-		if newItem.id != expectedID {
-			t.Errorf("Expected id %d but got %d", expectedID, newItem.id)
 		}
 
 		if repo.Root != newItem {
