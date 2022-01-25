cff606ee76fb4702f46266cfce14885299196c7e
Sam Lock <sam@swlock.co.uk>
2022-01-25 20:54:36 +0000

    Revert "`gather` optimisations"
    This reverts commit d2891247ff9e4a33c1ad1f11b3a2201722680b41.

--- pkg/service/debug.go
+++ pkg/service/debug.go
@@ -21,14 +21,7 @@ var eventNameMap = map[EventType]string{
 // DebugWriteEventsToFile is used for debug purposes. It prints all events for the given uuid/lamportTimestamp
 // combination to file, for inspection
 func (r *DBListRepo) DebugWriteEventsToFile(filename string, key string) {
-	el, _ := r.pull(context.Background(), func() <-chan WalFile {
-		ch := make(chan WalFile)
-		go func() {
-			defer close(ch)
-			ch <- r.LocalWalFile
-		}()
-		return ch
-	}())
+	el, _, _ := r.pull(context.Background(), []WalFile{r.LocalWalFile})
 
 	f, err := os.OpenFile(path.Join(filename, "results_"+key), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
